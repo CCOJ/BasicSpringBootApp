be7687b0eb13198e40ec84f1a4629d78b1f60827
CCOJ <randynguyen09@hotmail.com>
2018-05-15 18:21:04 -0500

    Beans

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,24 +1,37 @@
 package hello;
 
-import java.util.Arrays;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
-    
+
+    /**
+     * Launches the application
+     */
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * Runs on start up and retrieves all beans created.
+     * It sorts then prints out the beans.
+     */
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by the Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName: beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,12 +1,16 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * Web controller for the simple web application.
+ * Returns data in web requests instead of a view.
+ */
+@RestController //Means it's ready to be used by Spring MVC to handle web requests; combines @Controller & @ResponseBody
 public class HelloController {
-    
-    @RequestMapping("/")
+
+    @RequestMapping("/") //Maps "/" to the index() methods, when invoked it returns pure text
     public String index() {
         return "Greetings from Spring Boot!";
     }
